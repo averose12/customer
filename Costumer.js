a2dc486eed6516f25a44e4f2d7afb095d08571be
avee_12 <164250167+averose12@users.noreply.github.com>
2024-04-01 14:32:47 +0800

    Add files via upload

---
+++ Costumer.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function () {
+    $('#summary').hide();
+    $('#sbmt').click(function () {
+        $('#summary').show()
+        var item1 = parseInt($('#tv').val()) || 0;
+        var item2 = parseInt($('#vcr').val()) || 0;
+        var item3 = parseInt($('#remote').val()) || 0;
+        var item4 = parseInt($('#cd').val()) || 0;
+        var item5 = parseInt($('#tape').val()) || 0;
+        var price1 = parseFloat($('#pTV').text());
+        var price2 = parseFloat($('#pVCR').text());
+        var price3 = parseFloat($('#premote').text());
+        var price4 = parseFloat($('#pCD').text());
+        var price5 = parseFloat($('#ptape').text());
+        var result1 = item1 * price1;
+        var result2 = item2 * price2;
+        var result3 = item3 * price3;
+        var result4 = item4 * price4;
+        var result5 = item5 * price5;
+        var subtotal = result1 + result2 + result3 + result4 + result5;
+        var tax = subtotal * 0.0825;
+        var total = tax + subtotal;
+
+        
+        $('#bills').empty();
+        if (item1 > 0) {
+            $('#bills').append('<tr><td>' + item1 + '</td><td>' + $('#itm1').text() + '</td><td>' + price1 + '</td><td>' + result1.toFixed(2) + '</td></tr>');
+
+        }
+        if (item2 > 0) {
+            $('#bills').append('<tr><td>' + item2 + '</td><td>' + $('#itm2').text() + '</td><td>' + price2 + '</td><td>' + result2.toFixed(2) + '</td></tr>');
+
+        }
+        if (item3 > 0) {
+            $('#bills').append('<tr><td>' + item3 + '</td><td>' + $('#itm3').text() + '</td><td>' + price3 + '</td><td>' + result3.toFixed(2) + '</td></tr>');
+
+        }
+        if (item4 > 0) {
+            $('#bills').append('<tr><td>' + item4 + '</td><td>' + $('#itm4').text() + '</td><td>' + price4 + '</td><td>' + result4.toFixed(2) + '</td></tr>');
+
+        }
+        if (item5 > 0) {
+            $('#bills').append('<tr><td>' + item5 + '</td><td>' + $('#itm5').text() + '</td><td>' + price5 + '</td><td>' + result5.toFixed(2) + '</td></tr>');
+
+
+        }
+
+
+        $('#bills').append('<tr><td></td><td></td><th>SUBTOTAL:</td><td>' + subtotal.toFixed(2) + '</td></tr>');
+        $('#bills').append('<tr><td></td><td></td><th>TAX:</td><td>' + tax.toFixed(2) + '</td></tr>');
+        $('#bills').append('<tr><td></td><td></td><th>TOTAL:</td><td>' + total.toFixed(2) + '</td></tr>');
+        alert('The Total amount you will pay is ' + total.toFixed(2))
+    });
+
+
+});
---
+++ HomeController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETact.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Practice()
+        {
+            return View();
+        }
+        public ActionResult Practice1()
+        {
+            return View();
+        }
+        public ActionResult CustomerBill()
+        {
+            return View();
+        }
+        public ActionResult Sample()
+        {
+            return View();
+        }
+
+        public ActionResult Nice()
+        {
+            return View();
+        }
+        public ActionResult CustomerBills()
+        {
+            return View();
+        }
+        public ActionResult Triangle()
+        {
+            return View();
+        }
+        public ActionResult Grde()
+        {
+            return View();
+        }
+        public  ActionResult Speed()
+        {
+            return View();
+        }
+        public ActionResult Xample()
+        {
+            return View();
+        }
+        public ActionResult Sample2()
+        {
+            return View();
+        }
+        
+        public ActionResult Sample3()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Sirsample1()
+        {
+
+            return View();
+        }
+        public ActionResult Sirsample2()
+        {
+            var data = new List<object>();
+            var lastname = Request["lname"];
+            var firstname = Request["fname"];
+
+            data.Add(new
+            {
+                mess = 0
+            
+            });
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult SirEx()
+        {
+
+            return View();
+        }
+        public ActionResult SirEx1()
+        {
+            var data = new List<object>();
+            var firstname = Request["fname"];
+            var lastname = Request["lname"];
+            var genders = Request["Gendr"];
+            var provinces = Request["province"];
+            data.Add(new
+            {
+                mess = 1
+            });
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult StudentsAcc()
+        {
+            return View();
+        }
+       
+        public ActionResult StudAcc()
+        {
+            return View();
+        }
+         public ActionResult StudentsAcc1()
+        {
+            var data = new List<object>();
+            var idNumber = Request["IDnum"];
+            var LstName = Request["LastName"];
+            var FirstName = Request["FrstName"];
+            var Genderss = Request["Gnder"];
+            var Code = Request["CodeCourse"];
+            var Yrs = Request["Years"];
+            var Sub = Request["Subjects"];
+            var TUnits = Request["TotalUnits"];
+            var TuitionUnit = Request["TuitionPerUnit"];
+            var Overalltuition = Request["TotalTuition"];
+            var regfees = Request["Registration"];
+            var miscfees = Request["Miscellaneous"];
+            var Labfees = Request["LabFees"];
+            var TFeesCourse = Request["TotalFeesCourse"];
+            var Assess = Request["Assessment"];
+            var OverallTotal = Request["TotalFee"];
+            var bal = Request["Balance"];
+            var dollar = Request["wordsDollar"];
+            var cents = Request["wordsCents"];
+            var results = Request["Results"];
+            var AmountPayS = Request["AmountPay"];
+            data.Add(new
+            {
+                mess = 1
+            });
+            return Json(data, JsonRequestBehavior.AllowGet);
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
